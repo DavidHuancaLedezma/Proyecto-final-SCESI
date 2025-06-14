410f9f66c3402d38482e5f85abadcc77651aa70c
David <davidhuancaledezma@gmail.com>
2025-06-13 17:48:32 -0700

    feat: add informacion del usuario en celular

---
+++ src/components/usuarioCard/Usuario.jsx
@@ -0,0 +1,26 @@
+import { FaUser, FaCalendar, FaEnvelope, FaUserShield } from 'react-icons/fa';
+import style from './usuario.module.css';
+
+function Usuario () {
+    return(
+        <main className={style.contenedorUsuario}>
+            <header className={style.usuario}>
+                <span className={style.iconoContenedor}>
+                    <span className={style.icono}>
+                        <FaUser/>
+                    </span>
+                </span>
+                <h1 className={style.nombre}>Nombre del usuario</h1>
+                <p className={style.complemento}>usuario</p>
+            </header>
+            <section className={style.informacionCuenta}>
+                <h2 className={style.informacionTitulo}>Información de la cuenta</h2>
+                <hr />
+                <p className={style.informacion}><FaEnvelope className={style.iconoInformacion}/>usuario@gmail.com</p>
+                <p className={style.informacion}><FaCalendar className={style.iconoInformacion}/>Registrado el: 25 de mayo de 2025, 14:00PM</p>
+                <p className={style.informacion}><FaUserShield className={style.iconoInformacion}/>Rol:Usuario</p>
+            </section>
+        </main>
+    );
+}
+export default Usuario;
--- src/pages/Perfil.jsx
+++ src/pages/Perfil.jsx
@@ -1,8 +1,9 @@
 import LayoutBarra from '../layout/LayoutBarra';
+import Usuario from '../components/usuarioCard/usuario';
 function Perfil() {
   return (
     <LayoutBarra>
-      <p>los perfiles</p>
+      <Usuario/>
     </LayoutBarra>
   );
 }
