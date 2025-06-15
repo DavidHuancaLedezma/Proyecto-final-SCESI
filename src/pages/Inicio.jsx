5b35174c0ca896295afa3478c7797d17a97200b4
David <davidhuancaledezma@gmail.com>
2025-06-15 14:24:26 -0700

    style: add informacion de inicio responsive para pc

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -8,39 +8,39 @@ function Inicio() {
   return (
       <div className={style.publicidad}>
         <header className={style.portada}>
-          <section className={style.contenedortitulo}>
+          <section className={style.contenedorTitulo}>
             <h1 className={style.titulo}>Bienvenido a Fragmentos</h1>
             <figure className={style.presentacion}>
               <div className={style.icono}>
                 <img src={logo} alt="pequeña foto para la portada" />
               </div>
               <figcaption className={style.descripcion}>
-                <p className={style.descripciontitulo}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque sed aliquid unde at voluptatum optio soluta, tempore molestias molestiae, suscipit eum repudiandae, a nobis! Suscipit facere necessitatibus exercitationem soluta eveniet.</p>
+                <p className={style.descripcionTitulo}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque sed aliquid unde at voluptatum optio soluta, tempore molestias molestiae, suscipit eum repudiandae, a nobis! Suscipit facere necessitatibus exercitationem soluta eveniet.</p>
               </figcaption>
             </figure>
           </section>
         </header>
         <main>
           <section className={style.ambientes}>
-            <figure className={style.contenedorfotos}>
+            <figure className={style.contenedorFotos}>
               <img className={style.foto} src={ambientes1} alt="" />
-              <figcaption className={style.descripcionfotos}>
+              <figcaption className={style.descripcionFotos}>
                 <h2>Entrada</h2>
                 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores delectus ullam labore quae nam ad voluptate vitae maxime quos? Odit aspernatur hic nihil ab in necessitatibus suscipit autem explicabo omnis?</figcaption>
             </figure>
           </section>
           <section className={style.ambientes}>
-            <figure className={style.contenedorfotos}>
+            <figure className={style.contenedorFotos}>
               <img className={style.foto} src={ambientes2} alt="" />
-              <figcaption className={style.descripcionfotos}>
+              <figcaption className={style.descripcionFotos}>
                 <h2>Mesas</h2>
                 Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est vel iste obcaecati ullam, ex eligendi. Vitae, ut! Accusamus optio perferendis doloribus numquam maiores ipsa error vitae natus cupiditate! Consectetur, ipsum!</figcaption>
             </figure>
           </section>
           <section className={style.ambientes}>
-            <figure className={style.contenedorfotos}>
+            <figure className={style.contenedorFotos}>
               <img className={style.foto} src={ambientes3} alt="" />
-              <figcaption className={style.descripcionfotos}>
+              <figcaption className={style.descripcionFotos}>
                 <h2>Comidas</h2>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, ut? Deleniti voluptatibus dolor molestiae ab officiis, autem laudantium? Quis sequi fugit minima maxime hic consectetur fugiat ipsam sed corrupti aut.</figcaption>
             </figure>
@@ -48,20 +48,19 @@ function Inicio() {
           <section className={style.ambientes}>
             <div className={style.contenedorMapa}>
               <iframe className={style.mapa}
-              src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d475.94218043379885!2d-66.155644!3d-17.385977!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x93e37408d97cdf8f%3A0x42a91c4b11f1225f!2sSuassuna%20by%20Fragmentos!5e0!3m2!1ses-419!2sbo!4v1749945191972!5m2!1ses-419!2sbo" 
-              loading="lazy" 
-              referrerPolicy="no-referrer-when-downgrade">
-            </iframe>
-              <h2>Ubicación</h2>
-              <p className={style.descripcionfotos}>Encuentranos en nuestro restaurante, te esperamos.</p>
+                src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d475.94218043379885!2d-66.155644!3d-17.385977!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x93e37408d97cdf8f%3A0x42a91c4b11f1225f!2sSuassuna%20by%20Fragmentos!5e0!3m2!1ses-419!2sbo!4v1749945191972!5m2!1ses-419!2sbo" 
+                loading="lazy" 
+                referrerPolicy="no-referrer-when-downgrade">
+              </iframe>
+              <div className={style.descripcionFotos}>
+                <h2>Ubicación</h2>
+                Encuentranos en nuestro restaurante, esperamos que nos visites pronto, que esperas.
+              </div>
             </div>
-
           </section>
         </main>
         <footer className={style.referencia}>
-          <p className={style.nombrerestaurante}>Fagmentos</p>
-          <p>Disfruta de las mejores comidas</p>
-          <p className={style.derechosreservados}>&copy; 2025 Fragmentos. Todos los derechos reservados.</p>
+          <p className={style.derechosReservados}>&copy; 2025 Fragmentos. Todos los derechos reservados.</p>
         </footer>
       </div>
   );
